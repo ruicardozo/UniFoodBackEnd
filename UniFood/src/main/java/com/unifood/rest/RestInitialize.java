eb08d7b40ab8634e4f7697ec8fbc2d6a9ebb9ffa
Rui Cardozo <rui-cardozo@procergs.rs.gov.br>
2023-12-24 12:54:45 -0300

    Ativação da interface do Swagger para documentação e teste dos serviços

--- UniFood/src/main/java/com/unifood/rest/RestInitialize.java
+++ UniFood/src/main/java/com/unifood/rest/RestInitialize.java
@@ -3,6 +3,14 @@ package com.unifood.rest;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+	    info = @Info(title = "UniFood", version = "v1", description = "API REST do UniFood"),
+	    servers = @Server(url = "/UniFood/rest")
+	    )
 @ApplicationPath("/rest")
 public class RestInitialize extends Application
 {
